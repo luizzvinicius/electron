7e447a9d3cdb890b849bd9683e1f10c01bd9919e
Luiz Vinícius <viniciusadr244@gmail.com>
2024-02-01 09:27:09 -0300

    fix: remove wrong configuration

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <h1>Aqui eu vou construir meu app com vue</h1>
-    <p>Welcome to your Electron application.</p>
-</template>
-  
-<script setup>
-  console.log('👋 This message is being logged by "App.vue", included via Vite');
-</script>

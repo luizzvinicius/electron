a8e77a3afb7b060826f2dc7c682db8712ed489a2
Luiz Vinícius <viniciusadr244@gmail.com>
2024-02-27 10:28:31 -0300

    edit package json

--- electron/main.ts
+++ electron/main.ts
@@ -2,6 +2,7 @@ import { app, BrowserWindow, ipcMain } from "electron";
 import { shell } from "electron";
 import { exec } from "child_process";
 import path from "path";
+import isDev from "electron-is-dev";
 
 const createWindow = () => {
   return new BrowserWindow({
@@ -12,11 +13,11 @@ const createWindow = () => {
 };
 
 app.whenReady().then(() => {
-  try {
-    // shell.openPath(path.join(__dirname, "../executables/IPFS-Desktop-0.33.0.exe"));
-  } catch (e) {
-    console.log(e);
-  }
+  // try {
+  //   shell.openPath(path.join(__dirname, "../executables/IPFS-Desktop-0.33.0.exe"));
+  // } catch (e) {
+  //   console.log(e);
+  // }
   const main = createWindow();
   // const cluster = createWindow()
   // cluster.webContents.downloadURL("https://dist.ipfs.tech/ipfs-cluster-service/v1.0.8/ipfs-cluster-service_v1.0.8_windows-amd64.zip")
@@ -28,12 +29,17 @@ app.whenReady().then(() => {
     }
   );
   let bootstrap = exec(
-    `C:/Users/vinic/ipfs-cluster-service.exe daemon --bootstrap /ip4/172.19.0.6/tcp/9096/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp`,
+    "C:/Users/vinic/ipfs-cluster-service.exe -f daemon --bootstrap /ip4/127.0.0.1/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp",
+    // ipfs-cluster-service daemon --bootstrap /ip4/127.0.0.1/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp
     (error, stdout: string, stderr: string) => {
       console.log(error), console.log(stderr), console.log(stdout);
     }
   );
-  main.loadURL(process.env.VITE_DEV_SERVER_URL!);
+  main.loadURL(
+    isDev
+      ? process.env.VITE_DEV_SERVER_URL!
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
 });
 
 // escutar envento e chamar IPFS

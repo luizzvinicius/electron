0a2c915cf272e2a3068285f2708dd8ab94e17d5a
Luiz Vinícius <viniciusadr244@gmail.com>
2024-02-28 11:32:33 -0300

    feature: add files to cluster

--- electron/ipfs/func.ts
+++ electron/ipfs/func.ts
@@ -1,28 +1,38 @@
 import { create } from "kubo-rpc-client";
 import type { IPFSHTTPClient } from "kubo-rpc-client";
+import { Cluster } from "@nftstorage/ipfs-cluster";
 
-let client: IPFSHTTPClient;
-(function createClient() {
-  client = create({
+function createClient(): IPFSHTTPClient {
+  let client = create({
     host: "127.0.0.1",
     port: 5001,
   });
   return client;
-})()
+}
+
+function createCluster(): Cluster {
+  let cluster = new Cluster("http://127.0.0.1:9094");
+  return cluster;
+}
+
+let client = createClient();
+let cluster = createCluster();
 
 const uploadFile = async (file: File) => {
   try {
     const name = file.name;
-    const data2 = file.arrayBuffer.toString();
-    const formData = new FormData();
-    formData.append(name, data2);
-    client.files.write(`/${name}`, data2, { create: true });
+    const buff = await file.arrayBuffer();
+    await client.files.write(`/${name}`, new TextDecoder().decode(buff), { create: true });
 
     let info = [];
-    for await (const file of client.files.ls(`/${name}`)) {
-      info.push(file);
+    for await (const data of client.files.ls(`/${name}`)) {
+      info.push(data);
     }
-    console.log(info[0].cid.toString());
+    console.log("kubo: ", info[0].cid.toString());
+
+    const { cid } = await cluster.add(file);
+    await cluster.pin(cid)
+    console.log("cluster: ", cid);
   } catch (e) {
     console.log(e);
   }

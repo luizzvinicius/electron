7ea1c0c8dcd90e68b406481d1d42b27b6c080f8a
Luiz Vinicius <viniciusadr244@gmail.com>
2024-03-20 15:03:35 -0300

    update

--- electron/main.ts
+++ electron/main.ts
@@ -2,9 +2,9 @@ import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
 import { shell } from "electron";
 import { exec } from "child_process";
-import isDev from "electron-is-dev";
-
 import { fileURLToPath } from "url";
+const AdmZip = require("adm-zip");
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -30,38 +30,38 @@ process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 
 const preload = path.join(process.env.DIST, "preload.js");
 
-app.whenReady().then(() => {
-  // try {
-  //   shell.openPath(path.join(process.env.ROOT!, "release/win-unpacked/release/win-unpacked/resources/IPFS-Desktop-0.33.0.exe"));
-  // } catch (e) {
-  //   console.log(e);
-  // }
-  const main = createWindow();
-  // const cluster = createWindow()
-  // cluster.webContents.downloadURL("https://dist.ipfs.tech/ipfs-cluster-service/v1.0.8/ipfs-cluster-service_v1.0.8_windows-amd64.zip")
+const pathToEXE = "release/win-unpacked/resources/downloads/"
+app.whenReady().then(async () => {  
+  try {
+    shell.openPath(path.join(process.env.ROOT!, `${pathToEXE}IPFS-Desktop.exe`));
+    let zip = new AdmZip(path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service_v1.0.8_windows-amd64.zip`));
+    zip.extractAllTo(path.join(process.env.ROOT!, `${pathToEXE}`));
+    await shell.openPath(path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service/ipfs-cluster-service.exe`));
+  } catch (e) {
+    console.log(e);
+  }
+  
+  const main = createWindow();  
+  process.env.VITE_DEV_SERVER_URL
+    ? main.loadURL(process.env.VITE_DEV_SERVER_URL)
+    : main.loadFile(path.join(process.env.VITE_PUBLIC!, "index.html"));
 
   // let init = exec(
-  //   `C:/Users/vinic/ipfs-cluster-service.exe init -f http://bafybeigq7gjeusrv3tu7kaiv5yirr3vb76xqysjalwlec5fwkmudqx2bh4.ipfs.localhost:8080/`,
+  //   `${path.join(process.env.ROOT!, "release/win-unpacked/reources/app/dist/ipfs-cluster-service/ipfs-cluster-service.exe")} init -f http://bafybeigq7gjeusrv3tu7kaiv5yirr3vb76xqysjalwlec5fwkmudqx2bh4.ipfs.localhost:8080/`,
   //   (error, stdout: string, stderr: string) => {
   //     console.log(error), console.log(stderr), console.log(stdout);
   //   }
   // );
   // let bootstrap = exec(
-  //   "C:/Users/vinic/ipfs-cluster-service.exe -f daemon --bootstrap /ip4/127.0.0.1/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp",
+  //   `${path.join(process.env.ROOT!, "release/win-unpacked/reources/app/dist/ipfs-cluster-service/ipfs-cluster-service.exe")} -f daemon --bootstrap /ip4/127.0.0.1/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp`,
   //   // ipfs-cluster-service daemon --bootstrap /ip4/127.0.0.1/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp
   //   (error, stdout: string, stderr: string) => {
   //     console.log(error), console.log(stderr), console.log(stdout);
   //   }
   // );
-
-  if (process.env.VITE_DEV_SERVER_URL) {
-    main.loadURL(process.env.VITE_DEV_SERVER_URL);
-  } else {
-    main.loadFile(path.join(process.env.VITE_PUBLIC!, "index.html"))
-  }
 });
 
-ipcMain.on("click", (event, args) => {
-  console.log(args);
-  event.sender.send("click", "teste");
-});
+// ipcMain.on("click", (event, args) => {
+//   console.log(args);
+//   event.sender.send("click", "teste");
+// });

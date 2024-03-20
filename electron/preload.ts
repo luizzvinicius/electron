4de4d0beeb2f152d910bc7e40ea2311204644ecb
Luiz Vinicius <viniciusadr244@gmail.com>
2024-03-20 16:35:04 -0300

    verify OS

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,1 +1,20 @@
-console.log('---- electron/preload.ts ----')
+// import { BrowserWindow } from "electron";
+// import { platform } from "os";
+// import path from 'path'
+
+// const plataforma = platform();
+// let url: string = ''
+// console.log("Sistema Operacional:", plataforma);
+
+// if (plataforma === "win32") {
+//     url = "https://github.com/ipfs/ipfs-desktop/releases/download/v0.33.0/IPFS-Desktop-Setup-0.33.0.exe"
+// } else if (plataforma === "darwin") {
+//   url = "https://github.com/ipfs/ipfs-desktop/releases/download/v0.33.0/ipfs-desktop-0.33.0-mac.dmg"
+// } else if (plataforma === "linux") {
+//     url = "https://github.com/ipfs/ipfs-desktop/releases/download/v0.33.0/ipfs-desktop-0.33.0-linux-amd64.deb"
+// } else {
+//   console.log("Sistema Operacional não reconhecido:", plataforma);
+// }
+
+// const ipfs_download = createWindow();
+// ipfs_download.webContents.downloadURL(url);

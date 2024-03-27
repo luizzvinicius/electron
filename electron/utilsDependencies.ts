e95ae2a0377929edbf4796b72e8ae767b532ca9a
Luiz Vinicius <viniciusadr244@gmail.com>
2024-03-27 14:29:58 -0300

    add utilsDependencies

--- electron/utilsDependencies.ts
+++ electron/utilsDependencies.ts
@@ -1,22 +1,45 @@
 import path from "path";
 import { shell } from "electron";
-import {readFile, writeFile, existsSync} from 'fs';
+import { writeFile, existsSync } from "fs";
 import AdmZip from "adm-zip";
 
-
-async function criarArquivo(caminho: string) {
-    if (!existsSync(caminho)) {
-        const conteudoInicial = JSON.stringify({installed: true, IPFS_version: "0.33.0"}, null, 2)
-        writeFile(caminho, conteudoInicial, () => {});
-        console.log('Arquivo criado com sucesso.');
-    }
+async function createIfNotExists(caminho: string): Promise<boolean> {
+  if (!existsSync(caminho)) {
+    const date = new Date();
+    const conteudoInicial = JSON.stringify(
+      {
+        installed: true,
+        IPFS_version: "0.33.0",
+        Date: `${date.getDate()}/${
+          new Date().getMonth() + 1
+        }/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`,
+      },
+      null,
+      2
+    );
+    writeFile(caminho, conteudoInicial, () => {});
+    return true;
+  }
+  return false;
 }
 
-function installDependencies(caminho: string) {
-    shell.openPath(path.join(process.env.ROOT!, `${caminho}IPFS-Desktop-0.33.0.exe`));
-    let zip = new AdmZip(path.join(process.env.ROOT!, `${caminho}ipfs-cluster-service_v1.0.8_windows-amd64.zip`));
-    zip.extractAllTo(path.join(process.env.ROOT!, `${caminho}`));
-    shell.openPath(path.join(process.env.ROOT!, `${caminho}ipfs-cluster-service/ipfs-cluster-service.exe`));
+function installDependencies(caminho: string): void {
+  shell.openPath(
+    path.join(process.env.ROOT!, `${caminho}IPFS-Desktop-0.33.0.exe`)
+  );
+  let zip: AdmZip = new AdmZip(
+    path.join(
+      process.env.ROOT!,
+      `${caminho}ipfs-cluster-service_v1.0.8_windows-amd64.zip`
+    )
+  );
+  zip.extractAllTo(path.join(process.env.ROOT!, `${caminho}`));
+  shell.openPath(
+    path.join(
+      process.env.ROOT!,
+      `${caminho}ipfs-cluster-service/ipfs-cluster-service.exe`
+    )
+  );
 }
 
-export { criarArquivo, installDependencies }
+export { createIfNotExists, installDependencies };

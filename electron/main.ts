5b05551d302edef89456deb241b19ff74b61d0a4
Luiz Vinicius <viniciusadr244@gmail.com>
2024-03-27 14:20:52 -0300

    add types

--- electron/main.ts
+++ electron/main.ts
@@ -1,10 +1,9 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import path from "path";
-import { shell } from "electron";
 import { exec } from "child_process";
 import { fileURLToPath } from "url";
 import isDev from "electron-is-dev";
-const AdmZip = require("adm-zip");
+import { installDependencies, createIfNotExists } from "./utilsDependencies";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,33 +28,26 @@ process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL
   : path.join(process.env.ROOT, ".output/public");
 process.env["ELECTRON_DISABLE_SECURITY_WARNINGS"] = "true";
 
-const preload = path.join(process.env.DIST, "preload.js");
+const preload: string = path.join(process.env.DIST, "preload.js");
 
-const pathToEXE = isDev ? "release/win-unpacked/resources/downloads/" : "../../resources/downloads/"
+const pathToEXE: string = isDev ? "release/win-unpacked/resources/downloads/" : "../../resources/downloads/"
 app.whenReady().then(async () => {  
-  try {
-    shell.openPath(path.join(process.env.ROOT!, `${pathToEXE}IPFS-Desktop-0.33.0.exe`));
-    let zip = new AdmZip(path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service_v1.0.8_windows-amd64.zip`));
-    zip.extractAllTo(path.join(process.env.ROOT!, `${pathToEXE}`));
-    shell.openPath(path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service/ipfs-cluster-service.exe`));
-  } catch (e) {
-    console.log(e);
-  }
+  let exists: Promise<boolean> = createIfNotExists(path.join(process.env.ROOT!, pathToEXE, 'infoDependencies.json'))
+  if ( await exists ) installDependencies(pathToEXE)
   
-  const main = createWindow();  
+  const main: BrowserWindow = createWindow();  
   process.env.VITE_DEV_SERVER_URL
     ? main.loadURL(process.env.VITE_DEV_SERVER_URL)
     : main.loadFile(path.join(process.env.VITE_PUBLIC!, "index.html"));
 
   let init = exec(
-    `${path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service/ipfs-cluster-service.exe`)} init -f http://bafybeigq7gjeusrv3tu7kaiv5yirr3vb76xqysjalwlec5fwkmudqx2bh4.ipfs.localhost:8080/`,
+    `${path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service/ipfs-cluster-service.exe`)} init -f ${process.env.CLUSTER_CONFIG_URL}`,
     (error, stdout: string, stderr: string) => {
       console.log(error), console.log(stderr), console.log(stdout);
     }
   );
   let bootstrap = exec(
-    `${path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service/ipfs-cluster-service.exe`)} -f daemon --bootstrap /ip4/127.0.0.1/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp`,
-    // ipfs-cluster-service daemon --bootstrap /ip4/127.0.0.1/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp
+    `${path.join(process.env.ROOT!, `${pathToEXE}ipfs-cluster-service/ipfs-cluster-service.exe`)} -f daemon --bootstrap /ip4/${process.env.IPFS_URL}/tcp/9092/p2p/12D3KooWR8MxQgHGLF5vx1ShZy3Dkb5ZMjgm5MjVpJaDpi1Qqpwp`,
     (error, stdout: string, stderr: string) => {
       console.log(error), console.log(stderr), console.log(stdout);
     }
